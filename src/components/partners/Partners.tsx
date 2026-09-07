afe03dcba3cb5d380a56d1e8a5788f5a98c7547c
Arafath  Imorou <arafathimorou@gmail.com>
2026-09-07 14:58:46 +0100

    feat(partners): add ABMeD partner logo to partners carousel

--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -12,6 +12,7 @@ export const Partners: React.FC = () => {
     { name: "Techno Stat Group", src: "/images/partners/tsg.png" },
     { name: "ITA Innovate", src: "/images/partners/ita.jpg" },
     { name: "BHIC - Barika Health Impact Consulting", src: "/images/partners/bhic.jpg" },
+    { name: "ABMeD", src: "/images/partners/logoabmed.png" },
     { name: "Pharmacie Du Port", src: "/images/partners/pdp.png" },
     { name: "Pharmacie Gorobani", src: "/images/partners/phcie_gorobani.jpeg" },
     { name: "Logifroid", src: "/images/partners/logifroid.jpg", scale: "scale-[1.35]" },
