ebebc8ab695af80d2e8abf99d064fab47b79d21e
Arafath  Imorou <arafathimorou@gmail.com>
2026-09-08 10:57:20 +0100

    feat(partners): add MSH partner logo to partners carousel

--- src/components/partners/Partners.tsx
+++ src/components/partners/Partners.tsx
@@ -14,6 +14,7 @@ export const Partners: React.FC = () => {
     { name: "BHIC - Barika Health Impact Consulting", src: "/images/partners/bhic.jpg" },
     { name: "ABMeD", src: "/images/partners/logoabmed.png" },
     { name: "RegPharma", src: "/images/partners/logoregpharma.png" },
+    { name: "MSH - Management Sciences for Health", src: "/images/partners/logomsh.jpg", scale: "scale-[1.35]" },
     { name: "Pharmacie Du Port", src: "/images/partners/pdp.png" },
     { name: "Pharmacie Gorobani", src: "/images/partners/phcie_gorobani.jpeg" },
     { name: "Logifroid", src: "/images/partners/logifroid.jpg", scale: "scale-[1.35]" },
